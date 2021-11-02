ec4e5cd8f1fea1c36687980ed5d5867ec2b55880
Maksim Vikarchuk <m.vikarchuk@xpert.ch>
2021-11-02 18:49:04 +0300

    hw5

---
+++ src/main/java/hw5/Main.java
@@ -0,0 +1,16 @@
+package hw5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AppData appData = new AppData();
+        appData.load("test.txt");
+
+        System.out.println(Arrays.toString(appData.getHeader()));
+        System.out.println(Arrays.deepToString(appData.getData()));
+
+        appData.save("test.txt");
+    }
+}
